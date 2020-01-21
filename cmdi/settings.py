b873b6e77f515b9ad0b2ed09befa422c4f57caea
Eric Theise <erictheise@gmail.com>
2020-01-21 14:04:57 -0600

    Drop heroku-isms.

--- cmdi/settings.py
+++ cmdi/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 
 import os
 import django_heroku
+import dotenv
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -95,6 +96,7 @@ WSGI_APPLICATION = 'cmdi.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.contrib.gis.db.backends.postgis',
+        'NAME': 'cmdi',
     }
 }
 
