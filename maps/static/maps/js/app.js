a75ce2262850152b1c94586b1310256019cb80a6
Eric Theise <erictheise@gmail.com>
2020-02-19 14:26:06 -0800

    feat: addresses #1 by introducing simple feature clustering on map

--- maps/static/maps/js/app.js
+++ maps/static/maps/js/app.js
@@ -4,47 +4,251 @@ var map = new mapboxgl.Map({
   style: 'mapbox://styles/mapbox/dark-v10',
   center: [-74.5, 40], // starting position [lng, lat]
   zoom: 4,
-  hash: true
+  hash: true,
+  scrollZoom: false
 });
 
 let nav = new mapboxgl.NavigationControl({ showCompass: false });
-map.addControl(nav, 'bottom-left');
+map.addControl(nav, 'top-right');
 
 map.on('load', function () {
+  const
+    platformCoop = ['==', ['get', 'category'], 'platform co-op'],
+    coopRunPlatform = ['==', ['get', 'category'], 'co-op-run platform'],
+    sharedPlatform = ['==', ['get', 'category'], 'shared platform'],
+    supporter = ['==', ['get', 'category'], 'supporter'],
+    other = ['==', ['get', 'category'], ''],
+    colors = ['#e41a1c', '#377eb8', '#4daf4a', '#984ea3', '#ff7f00']
+  ;
+
+  const colorScale = d3.scaleOrdinal()
+    .domain('platformCoop', 'coopRunPlatform', 'sharedPlatform', 'supporter', 'other')
+    .range(colors)
+  ;
+
+  let
+    markers = {},
+    markersOnScreen = {},
+    point_counts = [],
+    totals
+  ;
+
   map.addSource('coops', {
-    type: 'geojson',
-    data: '/organizations/'
+    'type': 'geojson',
+    'data': '/organizations/',
+    'cluster': true,
+    'clusterMaxZoom': 14,
+    'clusterRadius': 50 //,
+    // 'clusterProperties': {
+    //   'platformCoop': ['+', ['case', platformCoop, 1, 0]],
+    //   'coopRunPlatform': ['+', ['case', coopRunPlatform, 1, 0]],
+    //   'sharedPlatform': ['+', ['case', sharedPlatform, 1, 0]],
+    //   'supporter': ['+', ['case', supporter, 1, 0]],
+    //   'other': ['+', ['case', other, 1, 0]]
+    // }
   });
+
   map.addLayer({
-    'id': 'coops',
-    'type': 'circle',
-    'source': 'coops',
+    id: 'clusters',
+    type: 'circle',
+    source: 'coops',
+    filter: ['has', 'point_count'],
+    // 'filter': ['!=', ['get', 'cluster'], true],
     'paint': {
-      'circle-opacity': 0.8,
-      'circle-radius': {
-        'base': 1.75,
-        'stops': [
-          [12, 12],
-          [22, 36]
-        ]
-      },
       'circle-color': [
-        'match',
-        ['get', 'category'],
-        'platform co-op', '#a6cee3',
-        'co-op-run platform', '#1f78b4',
-        'shared platform', '#b2df8a',
-        'supporter', '#33a02c',
-        '#ccc'
-      ]
+        'step',
+        ['get', 'point_count'],
+        '#51bbd6',
+        100,
+        '#f1f075',
+        750,
+        '#f28cb1'
+      ],
+      'circle-radius': [
+        'step',
+        ['get', 'point_count'],
+        20,
+        100,
+        30,
+        750,
+        40
+      ], //[
+      //   'case',
+      //   platformCoop, colorScale('platformCoop'),
+      //   coopRunPlatform, colorScale('coopRunPlatform'),
+      //   sharedPlatform, colorScale('sharedPlatform'),
+      //   supporter, colorScale('supporter'),
+      //   other, colorScale('other'),
+      //   '#999'
+      // ],
+      'circle-opacity': 0.8,
     }
   });
 
-  let popup = new mapboxgl.Popup({
-    closeButton: false,
-    closeOnClick: false
+  map.addLayer({
+    id: 'cluster-count',
+    type: 'symbol',
+    source: 'coops',
+    filter: ['has', 'point_count'],
+    layout: {
+      'text-field': '{point_count_abbreviated}',
+      // 'text-font': ['DIN Offc Pro Medium', 'Arial Unicode MS Bold'],
+      'text-size': 12
+    }
   });
 
+  map.addLayer({
+    id: 'unclustered-point',
+    type: 'circle',
+    source: 'coops',
+    filter: ['!', ['has', 'point_count']],
+    paint: {
+      'circle-color': '#11b4da',
+      'circle-radius': 4,
+      'circle-stroke-width': 1,
+      'circle-stroke-color': '#fff'
+    }
+  });
+
+  map.on('click', 'clusters', function (e) {
+    var features = map.queryRenderedFeatures(e.point, {
+      layers: ['clusters']
+    });
+    var clusterId = features[0].properties.cluster_id;
+    map.getSource('earthquakes').getClusterExpansionZoom(
+      clusterId,
+      function (err, zoom) {
+        if (err) return;
+
+        map.easeTo({
+          center: features[0].geometry.coordinates,
+          zoom: zoom
+        });
+      }
+    );
+  });
+
+  map.on('click', 'unclustered-point', function (e) {
+    map.getCanvas().style.cursor = 'pointer';
+    let
+      htmlString = '';
+      if (e.features[0].properties.url) {
+        htmlString += '<strong><a href="' + e.features[0].properties.url + '">' + e.features[0].properties.name + '</a></strong><br />'
+      } else {
+        htmlString += '<strong>' + e.features[0].properties.name + '</strong><br />'
+      }
+      if (e.features[0].properties.address) { htmlString += e.features[0].properties.address + '<br />' }
+      if (e.features[0].properties.city) { htmlString += e.features[0].properties.city + ' '}
+      if (e.features[0].properties.state) { htmlString += e.features[0].properties.state + ' ' }
+      if (e.features[0].properties.postal_code) { htmlString += e.features[0].properties.postal_code + ' ' }
+      if (e.features[0].properties.country) { htmlString += e.features[0].properties.country + ' ' }
+      if (e.features[0].properties.type || e.features[0].properties.category || e.features[0].properties.activities) {
+        htmlString += '<hr>'
+      }
+      if (e.features[0].properties.type) { htmlString += 'Type: ' + e.features[0].properties.type + '<br />'}
+      if (e.features[0].properties.category) { htmlString += 'Category: ' + e.features[0].properties.category + '<br />' }
+      if (e.features[0].properties.activities) {
+        htmlString += 'Sectors: ' + e.features[0].properties.activities.replace('[', '').replace(']', '').replace(/","/g, ', ').replace(/"/g, '');
+      }
+
+    let popup = new mapboxgl.Popup({
+      closeButton: true,
+      closeOnClick: true
+    });
+
+    popup.setLngLat(e.features[0].geometry.coordinates)
+      .setHTML(htmlString)
+      .addTo(map);
+  });
+
+  map.on('mouseenter', 'clusters', function () {
+    map.getCanvas().style.cursor = 'pointer';
+  });
+  map.on('mouseleave', 'clusters', function () {
+    map.getCanvas().style.cursor = '';
+  });
+
+  // map.addLayer({
+  //   'id': 'coops_individual_outer',
+  //   'type': 'circle',
+  //   'source': 'coops',
+  //   'filter': ['!=', ['get', 'cluster'], true],
+  //   'paint': {
+  //     'circle-color': [
+  //       'case',
+  //       platformCoop, colorScale('platformCoop'),
+  //       coopRunPlatform, colorScale('coopRunPlatform'),
+  //       sharedPlatform, colorScale('sharedPlatform'),
+  //       supporter, colorScale('supporter'),
+  //       other, colorScale('other'),
+  //       '#999'
+  //     ],
+  //     'circle-opacity': 1.0,
+  //     'circle-stroke-width': 2,
+  //     'circle-radius': 10,
+  //     'circle-color': "rgba(0, 0, 0, 0)"
+  //   }
+  // });
+
+  // const updateMarkers = () => {
+  //   // keep track of new markers
+  //   let newMarkers = {};
+  //   // get the features whether or not they are visible (https://docs.mapbox.com/mapbox-gl-js/api/#map#queryrenderedfeatures)
+  //   const features = map.querySourceFeatures('coops');
+  //   totals = getPointCount(features);
+  //   // loop through each feature
+  //   features.forEach((feature) => {
+  //     const coordinates = feature.geometry.coordinates;
+  //     // get our properties, which include our clustered properties
+  //     const props = feature.properties;
+  //     // continue only if the point is part of a cluster
+  //     if (!props.cluster) {
+  //       return;
+  //     }
+  //     // if yes, get the cluster_id
+  //     const id = props.cluster_id;
+  //     // create a marker object with the cluster_id as a key
+  //     let marker = markers[id];
+  //     // if that marker doesn't exist yet, create it
+  //     if (!marker) {
+  //       // create an html element (more on this later)
+  //       const el = createDonutChart(props, totals);
+  //       // create the marker object passing the html element and the coordinates
+  //       marker = markers[id] = new mapboxgl.Marker({
+  //         element: el
+  //       }).setLngLat(coordinates);
+  //     }
+  //
+  //     // create an object in our newMarkers object with our current marker representing the current cluster
+  //     newMarkers[id] = marker;
+  //
+  //     // if the marker isn't already on screen then add it to the map
+  //     if (!markersOnScreen[id]) {
+  //       marker.addTo(map);
+  //     }
+  //   });
+  //
+  //   // check if the marker with the cluster_id is already on the screen by iterating through our markersOnScreen object, which keeps track of that
+  //   for (id in markersOnScreen) {
+  //     // if there isn't a new marker with that id, then it's not visible, therefore remove it.
+  //     if (!newMarkers[id]) {
+  //       markersOnScreen[id].remove();
+  //     }
+  //   }
+  //   // otherwise, it is visible and we need to add it to our markersOnScreen object
+  //   markersOnScreen = newMarkers;
+  // };
+  //
+  // const getPointCount = (features) => {
+  //   features.forEach(f => {
+  //     if (f.properties.cluster) {
+  //       point_counts.push(f.properties.point_count)
+  //     }
+  //   });
+  //   return point_counts;
+  // };
+
+/*
   map.on('mouseover', 'coops', function (e) {
     map.getCanvas().style.cursor = 'pointer';
     let
@@ -75,5 +279,6 @@ map.on('load', function () {
     map.getCanvas().style.cursor = '';
     popup.remove();
   })
+*/
 
 });
