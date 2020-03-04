2b2d4e19b97c2f71dadf69e75d82631e243cbe20
Eric Theise <erictheise@gmail.com>
2020-03-04 10:41:34 -0600

    feat: addresses #6, start surfacing org profile

--- maps/views.py
+++ maps/views.py
@@ -1,6 +1,7 @@
 from django.http import HttpResponse
 from django.template import loader
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
+from mdi.models import Organization
 
 
 def index(request):
@@ -15,3 +16,11 @@ def profile(request):
     context = {
     }
     return HttpResponse(template.render(context, request))
+
+
+# Organization
+def organization_detail(request, organization_id):
+    organization = get_object_or_404(Organization, pk=organization_id)
+    context = {
+    }
+    return render(request, 'maps/organization_detail.html', {'organization': organization})
