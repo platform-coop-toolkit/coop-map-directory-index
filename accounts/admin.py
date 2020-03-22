1c69ba9aff97458b518e6b029eac9f81a1bb7100
Eric Theise <erictheise@gmail.com>
2020-03-21 20:27:18 -0600

    feat: addresses #7 add Role model (e.g. Founder, Member, Researcher…)

--- accounts/admin.py
+++ accounts/admin.py
@@ -2,7 +2,7 @@
 # from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from django.contrib.gis import admin
-from .models import User, UserSocialNetwork, Source
+from .models import User, UserSocialNetwork, Role, Source
 from django.db.models.functions import Lower
 
 
@@ -15,7 +15,7 @@ class UserSocialNetworkInline(admin.TabularInline):
 class CustomUserAdmin(UserAdmin):
     UserAdmin.fieldsets = (
         (None, {'fields': ('email', 'password')}),
-        ('Personal info', {'fields': ('first_name', 'middle_name', 'last_name', 'bio', 'city', 'state', 'postal_code', 'country', 'url', 'geom', 'source')}),
+        ('Personal info', {'fields': ('first_name', 'middle_name', 'last_name', 'bio', 'city', 'state', 'postal_code', 'country', 'url', 'geom', 'role', 'source')}),
         ('Free text fields', {'fields': ('notes',)}),
         ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
         ('Important dates', {'fields': ('last_login', 'date_joined')}),
@@ -25,6 +25,11 @@ class CustomUserAdmin(UserAdmin):
 
 
 admin.site.register(User, CustomUserAdmin)
+
+
+@admin.register(Role)
+class RoleAdmin(admin.ModelAdmin):
+    list_display = ('name', 'order', 'description')
 
 
 @admin.register(Source)
