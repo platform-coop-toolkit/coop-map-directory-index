581af4f2d4c7e3d2c3692e562666e9fb5bc4b3fc
Eric Theise <erictheise@gmail.com>
2020-03-29 21:07:14 -0700

    feat: closes #23, adds Swagger/OpenAPI

--- cmdi/settings.py
+++ cmdi/settings.py
@@ -58,6 +58,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'rest_framework_gis',
     'drf_yasg',
+    'corsheaders',
     'django_countries',
     'allauth',
     'allauth.account',
@@ -79,6 +80,8 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -89,14 +92,17 @@ MIDDLEWARE = [
     'lockdown.middleware.LockdownMiddleware',
 ]
 
+CORS_ORIGIN_ALLOW_ALL = True
+
 # django-lockdown is being used to deter the curious during development.
 # Keep it after launch should site access ever need to be disabled due to some emergency.
 LOCKDOWN_PASSWORDS = ('six', '6',)
 LOCKDOWN_ENABLED = True # Default is True. A quick way to make the site inaccessible.
 LOCKDOWN_URL_EXCEPTIONS = (
-    r'^/organizations/$',
-    r'^/sectors/$',
-    r'^/tools/$',
+    r'^/swagger.json$',
+    r'^/api/organizations/.*$',
+    r'^/api/sectors/.*$',
+    r'^/api/tools/.*$',
 )
 
 ROOT_URLCONF = 'cmdi.urls'
