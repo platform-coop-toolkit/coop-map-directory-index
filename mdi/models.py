c0227e55836c89fafec1e3757a5d010d48b40f07
Ned Zimmerman <ned@bight.dev>
2020-06-26 11:13:10 -0600

    fix: localize some tool choice strings, set defaults to empty

--- mdi/models.py
+++ mdi/models.py
@@ -208,18 +208,18 @@ class Tool(models.Model):
     url = models.URLField(blank=False, max_length=255)
     license_type = models.CharField(blank=True, default='', max_length=64,
                                     choices=[
-                                        ('unknown', 'Not sure'),
-                                        ('proprietary', 'Proprietary'),
-                                        ('proprietary-with-floss-integration-tools', 'Proprietary with free / libre / open source integration tools'),
-                                        ('floss', 'Free / libre / open source')
+                                        ('', _('Not sure')),
+                                        ('proprietary', _('Proprietary')),
+                                        ('proprietary-with-floss-integration-tools', _('Proprietary with free / libre / open source integration tools')),
+                                        ('floss', _('Free / libre / open source'))
                                     ],
-                                    verbose_name='License type')
-    license = models.ForeignKey(License, blank=True, null=True, on_delete=models.CASCADE, verbose_name='Free / libre / open source license')
+                                    verbose_name=_('License type'))
+    license = models.ForeignKey(License, blank=True, null=True, on_delete=models.CASCADE, verbose_name=_('Free / libre / open source license'))
     pricing = models.ForeignKey(Pricing, blank=True, null=True, on_delete=models.CASCADE)
     niches = models.ManyToManyField(Niche)
     languages_supported = models.ManyToManyField(Language, blank=True)
     sectors = models.ManyToManyField(Sector, blank=True)
-    coop_made = models.CharField(blank=False, default=0, max_length=16, choices=[('unknown', 'Not sure'), ('yes', 'Yes'), ('no', 'No')], verbose_name='Made by a cooperative')
+    coop_made = models.CharField(blank=True, default='', max_length=16, choices=[('', _('Not sure')), ('yes', _('Yes')), ('no', _('No'))], verbose_name=_('Made by a cooperative'))
     notes = models.TextField(blank=True, default='')
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
