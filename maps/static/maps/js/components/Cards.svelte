be287a1632f44c168b3b802ef6a2de74cff648e3
Ned Zimmerman <ned@bight.dev>
2020-08-07 09:31:59 -0300

    feat: add individual cards

--- maps/static/maps/js/components/Cards.svelte
+++ maps/static/maps/js/components/Cards.svelte
@@ -1,13 +1,56 @@
 <script>
 import { readable, get } from 'svelte/store';
 
-import Card from './Card.svelte';
-
 import { features } from '../store.js';
+
+import OrganizationCard from './OrganizationCard.svelte';
+import IndividualCard from './IndividualCard.svelte';
+
+function processOrganization(feature) {
+    const props = feature.properties;
+
+    props.id = feature.id;
+
+    props.type = (props.type === 'null') ? null : JSON.parse(props.type);
+
+    if (props.country !== '') {
+        props.country = JSON.parse(props.country);
+    }
+
+    props.categories = JSON.parse(props.categories);
+    props.languages = JSON.parse(props.languages);
+    props.sectors = JSON.parse(props.sectors);
+    props.socialnetworks = JSON.parse(props.socialnetworks);
+    props.tools = JSON.parse(props.tools);
+
+    return props;
+}
+
+function processIndividual(feature) {
+    const props = feature.properties;
+
+    props.id = feature.id;
+
+    props.name = `${props.first_name} ${props.last_name}`;
+
+    props.roles = JSON.parse(props.roles);
+
+    if (props.country !== '') {
+        props.country = JSON.parse(props.country);
+    }
+
+    props.languages = JSON.parse(props.languages);
+
+    return props;
+}
 </script>
 
 <ul class="cards">
     {#each $features as feature}
-    <Card organization={feature} />
+        {#if feature.source === 'organizations'}
+        <OrganizationCard organization={processOrganization(feature)} />
+        {:else if feature.source === 'individuals'}
+        <IndividualCard individual={processIndividual(feature)} />
+        {/if}
     {/each}
 </ul>
